8f559d8f75856b35ae80d77663fbf66f275e52e1
faith-cat <1355792668@qq.com>
2019-11-13 02:16:34 +0800

    Create GarbgeClassificationInferenceModel.java

---
+++ flink_package/yc_submit1/src/main/java/com/alibaba/tianchi/runMain/GarbgeClassificationInferenceModel.java
@@ -0,0 +1,10 @@
+package com.alibaba.tianchi.runMain;
+
+import com.intel.analytics.zoo.pipeline.inference.AbstractInferenceModel;
+
+public class GarbgeClassificationInferenceModel extends AbstractInferenceModel  {
+    public GarbgeClassificationInferenceModel() {
+        super();
+    }
+
+}
